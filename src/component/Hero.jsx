4d537f0a4f54cd17e917ecd7bc7ff207459a1e8c
Bamaramzy <22081010056@student.upnjatim.ac.id>
2025-02-09 06:16:09 +0700

    change a bit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -51,6 +51,7 @@ const Hero = () => {
                     initial={{ x: 100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }} 
                     transition={{ duration: 1, delay: 1.2 }}
+                    className="rounded-2xl"
                     src={profilePic} alt="Ramzy" />
                 </div>
             </div>
