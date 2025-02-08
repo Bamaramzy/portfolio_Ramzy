cc240ca430105ae3093a91880ab06f87adf4c04b
Bamaramzy <22081010056@student.upnjatim.ac.id>
2025-02-09 06:02:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import About from "./component/About";
+import Contact from "./component/Contact";
+import Experience from "./component/Experience";
+import Hero from "./component/Hero";
+import Navbar from "./component/navbar";
+import Projects from "./component/Projects";
+import Technologies from "./component/Technologies";
+
+const App = () => {
+  return (
+  <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-200 selection:text-cyan-900"> 
+    <div className="fixed top-0 -z-10 h-full w-full">
+    <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
+    </div>
+      <div className="container mx-auto px-8"></div>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Technologies/>
+        <Experience/>
+        <Projects/>
+        <Contact/>
+  </div>
+  );
+};
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,61 @@
+import project1 from "../assets/projects/project1.png";
+import project2 from "../assets/projects/project2.png";
+import project3 from "../assets/projects/project3.png";
+
+export const HERO_CONTENT = `I am a 6th semester student have a background in basic electronics and informatics and ever made an Arduino Uno-based lamp circuit that could be controlled via Bluetooth from a phone, assembled a 3-sound speaker
+system and designed a PCB layout for traffic lights, i also developed airline ticket booking website, car rental website, cek khodam website.`;
+
+export const ABOUT_TEXT = `I am a passionate and versatile developer with a strong foundation in electronics, informatics, and web development. As a 6th-semester student, I have worked on various projects, including an Arduino Uno-based lamp circuit controlled via Bluetooth, a three-sound speaker system, and a PCB layout for traffic lights. In the web development space, I have built an airline ticket booking website, a car rental website, and a cek khodam website. My curiosity for technology drives me to continuously learn, innovate, and solve real-world problems through both hardware and software solutions.`;
+export const EXPERIENCES = [
+  {
+    year: "2024",
+    role: "Junior Full Stack Developer",
+    company: "Binar Academy",
+    description: `As part of my Studi Independen at Binar Academy, my team and I developed a Car Rental Website, leveraging both frontend and backend development skills to create a functional and user-friendly platform. We used React.js for the frontend, Express.js for the backend, and PostgreSQL for database management.`,
+    technologies: ["React.js", "Express.js", "PostgreSQL"],
+  },
+  {
+    year: "2024",
+    role: "Junior Frontend Developer",
+    company: "Binar Academy",
+    description: `As part of my Studi Independen at Binar Academy, my team and I developed an Airline Ticket Booking Website, leveraging both frontend and backend development skills to create a functional and user-friendly platform. We used React.js for the frontend, Express.js for the backend, and PostgreSQL for database management.`,
+    technologies: ["React.js", "Bootstrap", "PostgreSQL"],
+  },
+  {
+    year: "2025",
+    role: "Junior Full Stack Developer",
+    company: "Self Project",
+    description: `I built and launched a Cek Khodam website on my own. It's a platform that allows users to check their khodam (it's just a for a joke!). The website is built using HTML, CSS, and JavaScript.`,
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Car Rental Website",
+    image: project1,
+    description:
+      "This project enhanced my full-stack development skills, strengthened my ability to work in a team, and provided hands-on experience with modern web development technologies.",
+    technologies: ["React.js", "Express.js", "PostgreSQL"],
+  },
+  {
+    title: "Airline Ticket Booking Website",
+    image: project2,
+    description:
+      "This project enhanced my full-stack development skills, strengthened my ability to work in a team, and provided hands-on experience with modern web development technologies. It's a platform that allows users to book tickets for flights.",
+    technologies: ["React.js", "Bootstrap", "PostgreSQL"],
+  },
+  {
+    title: "Cek Khodam Website",
+    image: project3,
+    description:
+      "Cek Khodam Website is an online platform that helps users check and identify their spiritual guides (just for a joke !).",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Surabaya, Indonesia",
+  phoneNo: "+62 8170 3808 005 ",
+  email: "bamaramzy04@gmailcom",
+};
